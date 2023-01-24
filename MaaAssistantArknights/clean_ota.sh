1dfc20a081cfb9e5571bc094806d4be968c98af6
MistEO <mistereo@hotmail.com>
2023-01-24 20:00:13 +0800

    chore: 少保留点旧的OTA包

--- MaaAssistantArknights/clean_ota.sh
+++ MaaAssistantArknights/clean_ota.sh
@@ -1,11 +1,11 @@
 cd ~/OTA/MaaAssistantArknights/MaaRelease/releases/download
 
 # stable version
-ls -tr1 | grep -v "-" | head --lines -5 | xargs -L 1 -r rm -rfv
+ls -tr1 | grep -v "-" | head --lines -3 | xargs -L 1 -r rm -rfv
 # beta version
-ls -tr1 | grep "-" | grep -v ".g" | head --lines -5 | xargs -L 1 -r rm -rfv
+ls -tr1 | grep "-" | grep -v ".g" | head --lines -3 | xargs -L 1 -r rm -rfv
 # nightly version
-ls -tr1 | grep "-" | grep ".g" | head --lines -5 | xargs -L 1 -r rm -rfv
+ls -tr1 | grep "-" | grep ".g" | head --lines -3 | xargs -L 1 -r rm -rfv
 
 # 兜底策略
-ls -tr1 | head --lines -30 | xargs -L 1 -r rm -rfv
+ls -tr1 | head --lines -10 | xargs -L 1 -r rm -rfv
