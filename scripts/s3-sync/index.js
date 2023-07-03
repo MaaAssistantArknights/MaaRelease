02d0c4f15db38effb72d6669477c54eca8ca5a9b
AnnAngela <naganjue@vip.qq.com>
2023-07-03 11:13:23 +0800

    ci: tls1.3

--- scripts/s3-sync/index.js
+++ scripts/s3-sync/index.js
@@ -6,6 +6,7 @@ import thread from "../modules/getThreadNumber.js";
 // eslint-disable-next-line no-unused-vars
 import { Client } from "minio";
 import http2 from "http2";
+import tls from "tls";
 import os from "os";
 import timerPromises from "timers/promises";
 const owner = "MaaAssistantArknights";
@@ -112,7 +113,14 @@ await Promise.all(Array.from({ length: thread }).map(async (_, i) => {
             });
             console.info("[Thread", i, "]", "Get the downloadable link of", asset.name, ", start downloading");
             const url = new URL(response.headers.get("location"));
-            const client = http2.connect(url);
+            const client = http2.connect(url, {
+                createConnection: () => tls.connect({
+                    host: url.hostname,
+                    port: url.port ? +url.port : 443,
+                    minVersion: "TLSv1.3",
+                    maxVersion: "TLSv1.3",
+                }),
+            });
             const info = await new Promise((res, rej) => {
                 client.on("error", (err) => {
                     rej(err);
