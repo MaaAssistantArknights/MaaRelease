70aa0626ef55f2983a5e2a70b9eab85b65d4ff25
AnnAngela <naganjue@vip.qq.com>
2023-07-31 09:26:51 +0800

    ci: 简化 readDir (#39)
    * ci: Update readDir.js
    
    * chore: Update package.json

--- scripts/modules/readDir.js
+++ scripts/modules/readDir.js
@@ -5,5 +5,5 @@ import fs from "fs";
  * @param {boolean} [removePrefix]
  * @returns {Promise<string[]>}
  */
-const readDir = async (p, removePrefix = false) => (await Promise.all((await Promise.all((await fs.promises.readdir(p, { withFileTypes: true })).map((dirent) => path.join(p, dirent.name)).map(async (n) => [n, await fs.promises.stat(n)]))).filter(([, stat]) => stat.isFile() || stat.isDirectory()).map(async ([n, stat]) => stat.isFile() ? removePrefix ? n.replace(`${p}/`, "") : n : await readDir(path.join(n), removePrefix)))).flat(999);
+const readDir = async (p, removePrefix = false) => (await fs.promises.readdir(p, { recursive: true })).map((n) => removePrefix ? n : path.join(p, n));
 export default readDir;
