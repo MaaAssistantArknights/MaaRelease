1a88c504b07222edf9f510db329e10ceeda0812c
AnnAngela <naganjue@vip.qq.com>
2023-10-24 09:33:48 +0800

    CI: REMOVE DEBUG

--- scripts/AnnAngela/webhookForJenkins.js
+++ scripts/AnnAngela/webhookForJenkins.js
@@ -2,7 +2,6 @@ import console from "../modules/console.js";
 import retryableFetch from "../modules/retryableFetch.js";
 
 const Authorization = `Basic ${Buffer.from(process.env.WEBHOOK_SECRET, "utf-8").toString("base64")}`;
-console.info({ Authorization });
 const result = await (await retryableFetch(process.env.WEBHOOK_URL, {
     headers: {
         Authorization,
