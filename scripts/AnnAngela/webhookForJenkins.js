bb406ac911c7273c854586f50ded452f4dd354c0
AnnAngela <naganjue@vip.qq.com>
2023-10-24 10:17:22 +0800

    ci: update log

--- scripts/AnnAngela/webhookForJenkins.js
+++ scripts/AnnAngela/webhookForJenkins.js
@@ -2,12 +2,21 @@ import console from "../modules/console.js";
 import retryableFetch from "../modules/retryableFetch.js";
 
 const Authorization = `Basic ${Buffer.from(process.env.WEBHOOK_SECRET, "utf-8").toString("base64")}`;
-const result = await (await retryableFetch(process.env.WEBHOOK_URL, {
+const result = await retryableFetch(process.env.WEBHOOK_URL, {
     headers: {
         Authorization,
     },
     method: "POST",
-})).text();
-console.info("Result:", result);
+});
+const headersEntries = [...result.headers.entries()];
+const unsafeOrUnneedHeaders = [
+    "x-content-type-options",
+    "x-frame-options",
+    "x-instance-identity",
+];
+const safeHeadersEntries = headersEntries.filter(([name]) => name.startsWith("x-") && !unsafeOrUnneedHeaders.includes(name));
+console.info("Status:", result.status);
+console.info("Headers:", Object.fromEntries(safeHeadersEntries));
+console.info("Result:", await result.text());
 console.info("Done.");
 process.exit(0);
