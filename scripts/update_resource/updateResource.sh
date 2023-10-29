03d131a85e726e82a47c95a1a66716786cdf8610
AnnAngela <naganjue@vip.qq.com>
2023-10-29 21:17:48 +0800

    ci: 更新 updateResource.sh
    日志

--- scripts/update_resource/updateResource.sh
+++ scripts/update_resource/updateResource.sh
@@ -8,7 +8,7 @@ git fetch --force --no-tags --prune --update-head-ok  \
   '+HEAD:refs/remotes/origin/HEAD'
 git checkout --force refs/remotes/origin/HEAD
 
-echo ==================== update resource ====================
+echo '==================== update resource ===================='
 
 python3 ~/MaaRelease/scripts/update_resource/list.py \
   ~/MaaAssistantArknights/resource \
