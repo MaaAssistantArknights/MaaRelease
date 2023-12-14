58f6fdfdc1ecec51d526efe4cd243931395e5cb6
MistEO <mistereo@hotmail.com>
2023-12-14 22:14:27 +0800

    feat: clearn_ota 脚本删除更多的包

--- MaaAssistantArknights/clean_ota.sh
+++ MaaAssistantArknights/clean_ota.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
+
+clear_files() {
+    # stable version
+    ls -tr1 | grep -v "-" | head --lines -3 | xargs -L 1 -r rm -rfv
+    # beta version
+    ls -tr1 | grep "-" | grep -v ".g" | head --lines -3 | xargs -L 1 -r rm -rfv
+    # nightly version
+    ls -tr1 | grep "-" | grep ".g" | head --lines -3 | xargs -L 1 -r rm -rfv
+    
+    # 兜底策略
+    ls -tr1 | head --lines -10 | xargs -L 1 -r rm -rfv
+}
+
 cd ~/OTA/MaaAssistantArknights/MaaRelease/releases/download
+clear_files
 
-# stable version
-ls -tr1 | grep -v "-" | head --lines -3 | xargs -L 1 -r rm -rfv
-# beta version
-ls -tr1 | grep "-" | grep -v ".g" | head --lines -3 | xargs -L 1 -r rm -rfv
-# nightly version
-ls -tr1 | grep "-" | grep ".g" | head --lines -3 | xargs -L 1 -r rm -rfv
-
-# 兜底策略
-ls -tr1 | head --lines -10 | xargs -L 1 -r rm -rfv
+cd ~/OTA/MaaAssistantArknights/MaaAssistantArknights/releases/download
+clear_files
