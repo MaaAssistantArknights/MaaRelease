59a8a2d494baad00c0f23b0b7f65ab8738c2a903
AnnAngela <naganjue@vip.qq.com>
2024-04-09 11:12:30 +0800

    feat: minio

--- scripts/eslint.config.js
+++ scripts/eslint.config.js
@@ -36,13 +36,31 @@ const config = [
         ],
         ignores,
     },
-    // Running in trusted environment
     {
         rules: {
+            // Running in trusted environment
             "security/detect-unsafe-regex": "off",
             "security/detect-object-injection": "off",
             "security/detect-non-literal-fs-filename": "off",
             "security/detect-non-literal-regexp": "off",
+
+            // github api use underscores naming
+            camelcase: [
+                "error",
+                {
+                    allow: [
+                        "pull_number",
+                        "issue_number",
+                        "head_commit",
+                        "commit_long",
+                        "state_reason",
+                        "workflow_id",
+                        "exclude_pull_requests",
+                        "per_page",
+                        "workflow_runs",
+                    ],
+                },
+            ],
         },
     },
 ];
