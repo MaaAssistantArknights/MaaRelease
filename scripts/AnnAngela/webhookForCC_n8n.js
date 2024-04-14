f9f227054ce12bd843a2565c15c97001849326de
AnnAngela <naganjue@vip.qq.com>
2024-04-14 17:48:54 +0800

    ci: add CC

---
+++ scripts/AnnAngela/webhookForCC_n8n.js
@@ -0,0 +1,24 @@
+import console from "../modules/console.js";
+import retryableFetch from "../modules/retryableFetch.js";
+
+const result = await retryableFetch(process.env.WEBHOOK_URL, {
+    headers: {
+        Authorization: process.env.WEBHOOK_SECRET,
+        "Content-Type": "application/json",
+    },
+    method: "PUT",
+    body: JSON.stringify({
+        RELEASE_TAG: process.env.RELEASE_TAG,
+    }),
+});
+const headersEntries = [...result.headers.entries()];
+const unsafeOrUnneedHeaders = [
+    "x-content-type-options",
+    "x-frame-options",
+    "x-instance-identity",
+];
+const safeHeadersEntries = headersEntries.filter(([name]) => name.startsWith("x-") && !unsafeOrUnneedHeaders.includes(name));
+console.info("Status:", result.status);
+console.info("Headers:", Object.fromEntries(safeHeadersEntries));
+console.info("Result:", await result.text());
+console.info("Done.");
